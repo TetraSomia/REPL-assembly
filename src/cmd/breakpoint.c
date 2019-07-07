82df21112043115b5893f6b5571e0faac375414c
Arthur Josso <arthur.josso@epitech.eu>
2019-07-07 18:48:23 +0200

    breakpoint

--- src/cmd/breakpoint.c
+++ src/cmd/breakpoint.c
@@ -1,7 +1,22 @@
+#include <string.h>
 #include "repl.h"
+#include "getters.h"
 
 int cmd_breakpoint(int ac, char **av) {
-  (void)ac;
-  (void)av;
+  u_parsed_val inst_v;
+  s_code_instruction *inst;
+
+  if (ac < 1)
+    return p_error("Please specify an instruction (index or address)\n");
+  if (ac > 1 && strcmp(av[1], "!") != 0)
+    return p_error("\'%s\' must be \'!\' or shouldn't be provided\n", av[1]);
+  if (get_addr_or_idx(av[0], &inst_v) != 0)
+    return p_error("\'%s\' cannot be parsed into an index or address\n", av[0]);
+  if ((inst = get_inst_from_parsing(&inst_v)) == NULL)
+    return 1;
+  if (ac == 1)
+    set_breakpoint(inst);
+  else
+    reset_breakpoint(inst);
   return 0;
 }
